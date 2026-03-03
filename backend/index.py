76c6e401a539e769f0f92ca0514c5b31b4600897
TarunTechie <taruntechie25@gmail.com>
2026-03-04 01:31:31 +0530

    basic structure of frontend added with fonts , colors ,backend api config and cors policy added in backend

--- backend/index.py
+++ backend/index.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from fileOps import router as fileRouter
 from dbOps import router as dbRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(fileRouter)
 app.include_router(dbRouter)
 
